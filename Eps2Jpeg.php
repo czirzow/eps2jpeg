8c962cb9b7e9f639302a80037384d74742e5f9f5
Curt Zirzow <czirzow@gmail.com>
2012-07-26 15:04:23 -0600

    Add basic files.

---
+++ Eps2Jpeg.php
@@ -0,0 +1,94 @@
+<?php
+
+class EpsToJpeg {
+	private static $pstill_cmd = '/usr/bin/pstill/pstill';
+	private static $convert_cmd = '/usr/bin/convert';
+	private static $identify_cmd = '/usr/bin/identify';
+
+	private $file = null;
+	private $width = null;
+	private $height = null;
+
+	public function __construct($eps_file, $eps_width=null, $eps_height=null) {
+		$this->file = $eps_file;
+		$this->width = $eps_width;
+		$this->height = $eps_height;
+	}
+
+	private function setSize() {
+		$cmd = self::$identify_cmd . ' ' . escapeshellarg($this->file);
+		exec("$cmd 2>&1", $output, $return_var);
+		if($return_var) {
+			return false;
+		}
+		// 'admin_1326964.eps EPT 823x648 823x648+0+0 DirectClass 2mb'
+		if(! preg_match('/ (\d+)x(\d+) /', $output[0], $matches)) {
+			return false;
+		}
+
+		$this->width = $matches[1];
+		$this->height = $matches[2];
+
+		return true;
+		
+	}
+
+	public function convert($jpg_size=null) {
+
+		if(! ($this->width || $this->height) ) {
+			//echo 'getting size..';
+
+			if(! $this->setSize() ) {
+				$this->error = 'Could not find size of eps';
+				return false;
+			}
+		}
+		
+		if(! $jpg_size) {
+			$ratio = '1.0';
+		} else {
+
+			if($this->width > $this->height) {
+				$ratio = number_format($jpg_size/$this->width, 2);
+				$convert_ratio = $jpg_size;
+			} else {
+				$ratio = number_format($jpg_size/$this->height, 2);
+				$convert_ratio = 'x' . $jpg_size;
+			}
+		}
+
+		$pdf_out = tempnam('/tmp/', 'eps2pdf');
+		$pstill =  self::$pstill_cmd . " -M pagescale=$ratio,$ratio  -M defaultall -s -p -m XPDFA=RGB -o $pdf_out " . escapeshellarg($this->file);
+		
+		exec("$pstill 2>&1", $output, $return_var);
+		if($return_var) {
+			unlink($pdf_out);
+			$this->error = "Could not prepare\n";
+			return false;
+		}
+		error_log($pstill);
+		//echo "$pstill\n"; var_dump($output);
+
+		$jpg_out = '/tmp/test_out.jpg';
+		$convert = self::$convert_cmd . " -resize $convert_ratio -antialias -quality 100   $pdf_out $jpg_out";
+		$output = array();
+		exec("$convert 2>&1", $output, $return_var);
+		error_log($convert);
+		if($return_var) {
+			error_log(print_r($outout, 1));
+			$this->error = "Could not convert\n";
+			return false;
+		}
+		//echo "$convert\n"; var_dump($output);
+
+		unlink($pdf_out);
+			
+		return $jpg_out;
+
+	}
+
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 eps2jpeg
+
+Required: 
+
+pstill 
+http://www.wizards.de/~frank/pstill.html
+http://www.wizards.de/~frank/pstill17814_linux_ia32.tar.gz
+
+Fonts:
+xorg-x11-fonts-Type1.noarch 7.1-2.1.el5      installed
 ========
 
-a
