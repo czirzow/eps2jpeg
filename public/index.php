b0ebaa6a2d2627121240a609aa811bda099050c3
Curt Zirzow <czirzow@shutterstock.com>
2012-10-03 03:01:57 +0400

    remove old response method.

--- public/index.php
+++ public/index.php
@@ -4,42 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../Eps2Jpeg.php';
 
 
-class Respond {
-	const BAD       = -2;
-	const UNKNOWN   = -1;
-	const PARAM     =  1;
-	const INIT      =  2;
-	const CONVERT   =  3;
-
-	private static $response_codes = array(
-		-2 => array('status' => 400, 'message' => 'Bad Upload'),
-		-1 => array('status' => 400, 'message' => 'Unknown error occured'),
-		 1 => array('status' => 400, 'message' => 'Invalid Parameter'),
-		 2 => array('status' => 400, 'message' => 'Problem with initializing parameters'),
-		 3 => array('status' => 400, 'message' => 'Problem Converting Image'),
-	);
-
-
-	public function withError($message, $code=-1) {
-		if(! $response = self::$response_codes[$code]) {
-			$response = self::$response_codes[-1];
-		}
-		$r = "HTTP/1.1 {$response['status']} [{$code}]:{$response['message']}";
-		header($r);
-		$output = array(
-			'status' => $response['status'],
-			'code' => $code,
-			'error' => $response['message'],
-			'message' => $message,
-		);
-		header('Content-Type: application/json; charset=UTF-8');
-		echo  json_encode($output);
-		exit;
-
-	}
-
-}
-
 $view = new \Slim\Extras\Views\Twig();
 
 	$app = new Slim\Slim(array(
